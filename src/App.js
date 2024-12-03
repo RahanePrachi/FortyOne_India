49a5e46aa6594cf2a915d635d31a4e40707113a7
PrachiRahane <prachirahane95@gmail.com>
2024-12-03 17:09:42 +0530

    other project component created

--- src/App.js
+++ src/App.js
@@ -3,15 +3,18 @@ import { Button } from 'react-bootstrap';
 import Header from './comman__components/header/Header'
 import Footer from './comman__components/footer/Footer';
 import { BrowserRouter as Router, Routes, Route ,Link} from 'react-router-dom';
+import Other_project from './components/core/project_dropdown/Other_project';
 function App() {
   return (
     <Router>
       <Header/>
+      <Other_project/>
     <Routes>
       {/* <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
       <Route path="/contact" element={<Contact />} /> */}
     </Routes>
+      
     <Footer/>
   </Router>
   );
---
+++ src/components/core/project_dropdown/Other_project.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Other_project = () => {
+  return (
+    <div className=' '>
+      <div className='w-screen bg-gradient-to-t from-green-300 via-transparent to-orange-300  flex items-center justify-center text-center h-[320px]  '>
+        <p className='font-montserrat font-extrabold text-[58px] leading-[80px]'>Other Projects</p>
+      </div>
+      <div className=' flex items-center justify-center  h-[400px] '>
+        <p className='font-montserrat font-extrabold text-7xl text-center  text-[#124F92]'>Project is comming soon</p>
+      </div>
+    </div>
+  )
+}
+
+export default Other_project;
